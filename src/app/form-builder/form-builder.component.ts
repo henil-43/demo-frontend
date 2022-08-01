ed628c2a7403ceb877207252866bd4584423e84d
agilepc115 <henil.r@agileinfoways.com>
2022-08-01 19:04:57 +0530

    completed implementing form-builder with drag and drop feature

--- src/app/form-builder/form-builder.component.ts
+++ src/app/form-builder/form-builder.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Sanitizer } from '@angular/core';
 import { CdkDragDrop, moveItemInArray, copyArrayItem} from '@angular/cdk/drag-drop'
+import { cloneDeep } from 'lodash';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-form-builder',
@@ -10,20 +12,21 @@ export class FormBuilderComponent implements OnInit {
 
   popup: any
   isVisible: Boolean = false
-  nextFun = false
 
   htmlItems:any = [
     {name: 'label', type: 'label', inputType: 'label', displayText: "sample"},
-    {name: 'text', type: 'input text', inputType: 'text', placeholder: ''},
+    {name: 'text', type: 'input text', inputType: 'text', placeholder: '', value:''},
+    {name: 'textarea', rows: '', cols:'', displayText: '', placeholder:''},
     {name: 'checkbox', type: 'input-check', inputType: 'checkbox', placeholder: '', displayText: ""},
-    {name: 'radio', type: 'input-check', inputType: 'radio', displayText: ""},
+    {name: 'radio', type: 'input-check', inputType: 'radio', displayText: "", value: ''},
+    {name: 'button', type: '', displayText: ""},
 
   ]
   htmlText: any
   droppedItems: any = []
   test: string = ''
   currentDraggedItem: any
-  constructor() { }
+  constructor(private santitizer: DomSanitizer) { }
 
   ngOnInit(): void {
     this.popup = {}
@@ -31,24 +34,46 @@ export class FormBuilderComponent implements OnInit {
 
   onSubmit(name: any){
     this.isVisible = false
-    if(this.currentDraggedItem.previousIndex == 0){
+    let temp = this.currentDraggedItem.previousContainer.data[this.currentDraggedItem.previousIndex].name 
+    if(temp == 'label'){
       this.currentDraggedItem.previousContainer.data[this.currentDraggedItem.previousIndex].displayText = name.value.name
 
     }
-    else if(this.currentDraggedItem.previousIndex == 1){
+    else if(temp == 'text'){
       this.currentDraggedItem.previousContainer.data[this.currentDraggedItem.previousIndex].placeholder = name.value.placeholder
     }
-    else if(this.currentDraggedItem.previousIndex == 2){
+    else if(temp == 'checkbox'){
       this.currentDraggedItem.previousContainer.data[this.currentDraggedItem.previousIndex].displayText = name.value.name
       this.currentDraggedItem.previousContainer.data[this.currentDraggedItem.previousIndex].placeholder = name.value.placeholder
     }
+    else if(temp == 'radio'){
+      this.currentDraggedItem.previousContainer.data[this.currentDraggedItem.previousIndex].displayText = name.value.name
+      this.currentDraggedItem.previousContainer.data[this.currentDraggedItem.previousIndex].value = name.value.value
+    }
+    else if(temp == 'button'){
+      this.currentDraggedItem.previousContainer.data[this.currentDraggedItem.previousIndex].displayText = name.value.name
+      this.currentDraggedItem.previousContainer.data[this.currentDraggedItem.previousIndex].type = name.value.type
+    }
+    else if(temp == 'textarea'){
+      this.currentDraggedItem.previousContainer.data[this.currentDraggedItem.previousIndex].displayText = name.value.name
+      this.currentDraggedItem.previousContainer.data[this.currentDraggedItem.previousIndex].placeholder = name.value.placeholder
+      this.currentDraggedItem.previousContainer.data[this.currentDraggedItem.previousIndex].rows = name.value.rows
+      this.currentDraggedItem.previousContainer.data[this.currentDraggedItem.previousIndex].cols = name.value.columns
+      
+      
+    }
+    
+    // copyArrayItem(
+    //   this.currentDraggedItem.previousContainer.data,
+    //   this.currentDraggedItem.container.data,
+    //   this.currentDraggedItem.previousIndex,
+    //   this.currentDraggedItem.currentIndex
+    // );
 
-    copyArrayItem(
-      this.currentDraggedItem.previousContainer.data,
-      this.currentDraggedItem.container.data,
-      this.currentDraggedItem.previousIndex,
-      this.currentDraggedItem.currentIndex
-    );
+    const clone = cloneDeep(this.currentDraggedItem.previousContainer.data[this.currentDraggedItem.previousIndex])
+    this.currentDraggedItem.container.data.splice(this.currentDraggedItem.currentIndex, 0, clone)
+
+    this.currentDraggedItem = {}
     name.reset()
 
   }
@@ -68,8 +93,43 @@ export class FormBuilderComponent implements OnInit {
       //   event.currentIndex,
       // );
       this.isVisible = true
-      this.currentDraggedItem = event
+      this.currentDraggedItem = {...event}
       console.log(event)
   }
+  }
+
+  generateHTMLCode():any{
+    
+    this.htmlText = "<form><div>"
+    console.log(this.droppedItems)
+    for (let i=0;i<this.droppedItems.length;i++){
+      if(this.droppedItems[i].name == 'label'){
+        this.htmlText += `<label>${this.droppedItems[i].displayText}</label>`
+      }
+      else if(this.droppedItems[i].name != 'label' && this.droppedItems[i].name != 'button' && this.droppedItems[i].name != 'textarea'){ 
+        this.htmlText += `<input type="${this.droppedItems[i].inputType}"` 
+         if(this.droppedItems[i].placeholder){
+          this.htmlText += `[placeholder]="${this.droppedItems[i].placeholder}"` 
+         }
+        if(this.droppedItems[i].displayText){
+          this.htmlText += `[name]="${this.droppedItems[i].displayText}"`
+        }
+        if(this.droppedItems[i].value){
+          this.htmlText += `[value]="${this.droppedItems[i].value}"`
+        }
+        this.htmlText += ">"
+      }
+      else if(this.droppedItems[i].name == 'button'){
+            
+        this.htmlText += `<button type="${this.droppedItems[i].type}">${this.droppedItems[i].displayText}</button>`
+      }
+      else if(this.droppedItems[i].name == 'textarea'){
+            
+        this.htmlText += `<textarea rows="${this.droppedItems[i].rows}" cols="${this.droppedItems[i].cols}" name="${this.droppedItems[i].displayText}">${this.droppedItems[i].placeholder}</textarea>`
+      }
+    }
+  
+    this.htmlText += "</form></div>"
+
   }
 }
